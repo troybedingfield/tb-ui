b0672b3e8b366e8a4601a67d57569ef609919f2a
troybedingfield <troybedingfield@gmail.com>
2024-12-07 15:31:51 -0800

    storybook/main.ts remove vite-plugin-dts from storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { withoutVitePlugins } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
   stories: [
@@ -15,6 +16,13 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  async viteFinal(config) {
+    config.plugins = await withoutVitePlugins(config.plugins, [
+      'vite:dts',
+    ]);
+
+    return config;
   },
 };
 export default config;
